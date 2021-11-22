8af92528e17a6c3effb3f8d6b2d4d9f5c21da949
hashir96 <micheltanga0@gmail.com>
2021-11-22 13:43:36 +0100

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+ firebase : {
+  apiKey: "AIzaSyB9H9EUISIvJpZNkgyvAEDVDR3K3n3ETKU",
+  authDomain: "message-d9555.firebaseapp.com",
+  databaseURL: "https://message-d9555.firebaseio.com",
+  projectId: "message-d9555",
+  storageBucket: "message-d9555.appspot.com",
+  messagingSenderId: "1066035286960",
+  appId: "1:1066035286960:web:11c2d0a8662609d867bc75",
+  measurementId: "G-SV73LBC14V"
+},
+};
